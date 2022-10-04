f71bba37a0b80cf6880b408f50d1a912c3275d69
Ernestas V <35690113+ihah@users.noreply.github.com>
2022-10-04 19:47:51 +0300

    Update products.php

--- products.php
+++ products.php
@@ -1,4 +1,18 @@
 <?php
 
-// TODO: Add product page
-// TODO: use Product::find($_GET['id']) to get product object or return error
+// require_once('./classes/Product');
+
+// TODO: add products list page
+// $products = Product::getProducts('./data/products.json');
+?>
+
+<html>
+  <head>
+    <title>
+      Products list
+    </title>
+  </head>
+  
+  <body>
+  </body>
+</html>
